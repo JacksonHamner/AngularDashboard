3e8be15f42993b5c0e70484fb525daa91dd590aa
jackson hamner <jacksonhamner@gmail.com>
2019-09-09 07:42:45 -0700

    Scaffolded out Sales Volume Page

--- angular-dashboard/src/app/charts/line-chart/line-chart.component.ts
+++ angular-dashboard/src/app/charts/line-chart/line-chart.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { LINE_CHART_COLORS } from '../../shared/chart.colors';
+
+const LINE_CHART_SAMPLE_DATA: any[] = [
+  { data: [32, 14, 46 ,89, 11 ,25], label: 'Sentiment Analysis' },
+  { data: [36, 55, 79 ,22, 36, 50], label: 'Image Recognition' },
+  { data: [99, 1, 54, 33, 32, 12], label: 'Forecasting' }
+];
+
+const LINE_CHART_LABELS: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May','Jun'];
 
 @Component({
   selector: 'app-line-chart',
@@ -8,6 +17,15 @@ import { Component, OnInit } from '@angular/core';
 export class LineChartComponent implements OnInit {
 
   constructor() { }
+
+  lineChartData = LINE_CHART_SAMPLE_DATA;
+  lineChartLabels = LINE_CHART_LABELS;
+  lineChartOptions: any = {
+    responsive: true
+  };
+  lineChartLegend: true;
+  lineChartType = 'line';
+  lineChartColors = LINE_CHART_COLORS;    
 
   ngOnInit() {
   }
