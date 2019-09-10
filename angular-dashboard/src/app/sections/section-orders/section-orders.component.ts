9283bc5d73f45855d23e8c80a756f7a117d14eff
jackson hamner <jacksonhamner@gmail.com>
2019-09-10 07:44:56 -0700

    latest orders page

--- angular-dashboard/src/app/sections/section-orders/section-orders.component.ts
+++ angular-dashboard/src/app/sections/section-orders/section-orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from 'src/app/shared/Order';
 
 @Component({
   selector: 'app-section-orders',
@@ -8,6 +9,80 @@ import { Component, OnInit } from '@angular/core';
 export class SectionOrdersComponent implements OnInit {
 
   constructor() { }
+
+  orders: Order[] = [
+    {
+      id: 1,
+      customer:
+      {
+        id: 1,
+        name: 'Main st Bakery',
+        state: 'CO',
+        email: 'mainstreet@example.com'
+      },
+      total: 230,
+      placed: new Date(2017, 12, 1),
+      fulfilled: new Date(2017, 12, 2),
+      status: 'completed'
+    },
+    {
+      id: 2,
+      customer:
+      {
+        id: 1,
+        name: 'Main st Bakery',
+        state: 'CO',
+        email: 'mainstreet@example.com'
+      },
+      total: 230,
+      placed: new Date(2017, 12, 1),
+      fulfilled: new Date(2017, 12, 2),
+      status: 'completed'
+    },
+    {
+      id: 3,
+      customer:
+      {
+        id: 1,
+        name: 'Main st Bakery',
+        state: 'CO',
+        email: 'mainstreet@example.com'
+      },
+      total: 230,
+      placed: new Date(2017, 12, 1),
+      fulfilled: new Date(2017, 12, 2),
+      status: 'completed'
+    },
+    {
+      id: 4,
+      customer:
+      {
+        id: 1,
+        name: 'Main st Bakery',
+        state: 'CO',
+        email: 'mainstreet@example.com'
+      },
+      total: 230,
+      placed: new Date(2017, 12, 1),
+      fulfilled: new Date(2017, 12, 2),
+      status: 'completed'
+    },
+    {
+      id: 5,
+      customer:
+      {
+        id: 1,
+        name: 'Main st Bakery',
+        state: 'CO',
+        email: 'mainstreet@example.com'
+      },
+      total: 230,
+      placed: new Date(2017, 12, 1),
+      fulfilled: new Date(2017, 12, 2),
+      status: 'completed'
+    }
+  ];
+
 
   ngOnInit() {
   }
