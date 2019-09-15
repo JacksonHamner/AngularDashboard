086db8801517066d3ef208026a4f44077efcfafb
jackson hamner <jacksonhamner@gmail.com>
2019-09-15 10:47:37 -0700

    created system health section

--- angular-dashboard/src/app/app.module.ts
+++ angular-dashboard/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { SectionHealthComponent } from './sections/section-health/section-health
 import { BarChartComponent } from './charts/bar-chart/bar-chart.component';
 import { LineChartComponent } from './charts/line-chart/line-chart.component';
 import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
+import { ServerComponent } from './server/server.component';
 
 
 @NgModule({
@@ -24,7 +25,8 @@ import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
     SectionHealthComponent,
     BarChartComponent,
     LineChartComponent,
-    PieChartComponent
+    PieChartComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
